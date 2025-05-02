5d2db759b8f8ee6c3b9364d8cf8e9c66665aa16f
Courtney Ross <cross52@student.gsu.edu>
2025-05-02 19:59:40 -0400

    added stock ticks api for user chart usage

--- lib/screens/stockhandling.dart
+++ lib/screens/stockhandling.dart
@@ -28,3 +28,30 @@ Future<double> getprice(String symbol) async {
     throw Exception('API Handling Error');
   }
 }
+
+Future<List<Map<String, dynamic>>> getHistoricalData(String symbol) async {
+  final url = Uri.parse(
+    'https://finnhub.io/api/v1/stock/candle?symbol=$symbol&resolution=D&from=1680307200&to=1682899200&token=$api',
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    if (data['s'] == 'ok') {
+      final timestamps = data['t'] as List<dynamic>;
+      final prices = data['c'] as List<dynamic>;
+
+      return List.generate(
+        timestamps.length,
+        (index) => {
+          'time': DateTime.fromMillisecondsSinceEpoch(timestamps[index] * 1000),
+          'price': prices[index],
+        },
+      );
+    } else {
+      throw Exception('No data available');
+    }
+  } else {
+    throw Exception('API Handling Error');
+  }
+}
