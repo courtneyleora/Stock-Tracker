c80ed54211bf665041c92f3dd21f0240b31dd103
Skyler Plumley <skylerplumley@gmail.com>
2025-05-02 21:56:08 -0400

    Aesthetic changes made, user is greeted with first name registered

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -17,6 +17,27 @@ class _MainScreenState extends State<MainScreen> {
   String? stocksymbol;
   String? stockprice;
   String? error;
+  String? firstName;
+
+  @override
+  void initState() {
+    super.initState();
+    getname();
+  }
+
+  Future<void> getname() async {
+    if (user != null) {
+      final doc =
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user!.uid)
+              .get();
+
+      setState(() {
+        firstName = doc.data()?['firstName'] ?? 'User';
+      });
+    }
+  }
 
   Future<void> searchstock() async {
     final symbol = search.text.trim().toUpperCase();
@@ -64,8 +85,7 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final displayName = user?.email ?? "User";
-
+    final displayName = firstName ?? user?.email ?? "User";
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightGreen[300],
@@ -79,71 +99,145 @@ class _MainScreenState extends State<MainScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SingleChildScrollView(
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => WatchlistScreen(),
-                        ),
-                      );
-                    },
-                    child: Text("Watchlist"),
+            Wrap(
+              spacing: 16,
+              alignment: WrapAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WatchlistScreen(),
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.star_border, color: Colors.lightGreen[600]),
+                  label: Text("Watchlist"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.lightGreen,
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                   ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text("Charts"),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  icon: Icon(Icons.bar_chart, color: Colors.lightGreen[600]),
+                  label: Text("Charts"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.lightGreen,
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                   ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text("News Feed"),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  icon: Icon(
+                    Icons.article_outlined,
+                    color: Colors.lightGreen[600],
                   ),
-                ],
-              ),
+                  label: Text("Newsfeed"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.lightGreen,
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 15),
             TextField(
               controller: search,
               decoration: InputDecoration(
                 hintText: "Enter Stock Name by Symbol",
-                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white,
+                suffixIcon: IconButton(
+                  onPressed: () => search.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
             SizedBox(height: 12),
-            ElevatedButton(onPressed: searchstock, child: Text("Search")),
+            ElevatedButton.icon(
+              onPressed: searchstock,
+              icon: Icon(Icons.search, color: Colors.lightGreen[100]),
+              label: Text("Search"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightGreen[600],
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              ),
+            ),
             SizedBox(height: 16),
             if (error != null)
               Text(error!, style: TextStyle(color: Colors.red)),
             if (stockprice != null)
               Card(
+                elevation: 4,
                 margin: EdgeInsets.symmetric(vertical: 10),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(15),
                 ),
                 child: ListTile(
+                  leading: Icon(Icons.trending_up),
                   title: Text(
                     stocksymbol!,
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  subtitle: Text(stockprice!, style: TextStyle(fontSize: 15)),
-                  trailing: IconButton(
+                  subtitle: Text(
+                    stockprice!,
+                    style: TextStyle(fontSize: 15, color: Colors.grey[600]),
+                  ),
+                  trailing: ElevatedButton.icon(
+                    label: Text("Add Stock"),
                     onPressed: addtowatchlist,
-                    icon: Icon(Icons.add, color: Colors.green),
+                    icon: Icon(Icons.add_circle_outline, color: Colors.green),
                   ),
                 ),
               ),
